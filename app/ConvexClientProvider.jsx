fe54776a5dc207b5dfdc916958ed2db62762cf5e
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2025-04-06 12:26:04 +0530

    project setup

--- app/ConvexClientProvider.jsx
+++ app/ConvexClientProvider.jsx
@@ -1,11 +1,18 @@
 "use client";
 import React from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react"; // ✅ Required imports
-
+import Provider from "./provider";
 const ConvexClientProvider = ({ children }) => {
   const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexProvider client={convex}>
+      <Provider>
+        {children}
+      </Provider>
+      {children}
+      </ConvexProvider>
+  )
 };
 
 export default ConvexClientProvider;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { UserButton } from "@stackframe/stack";
 import Image from "next/image";
 
 export default function Home() {
   return (
   <>
   hii
+  <UserButton/>
   </>
   );
 }
